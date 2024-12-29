2f72d3abaa32731facdfdcbe5f012fb4c1b78920
FerreteriaSuperior <FerreteriaSuperior.fs@gmail.com>
2024-12-29 12:14:25 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -3,48 +3,67 @@ let next = document.getElementById('next');
 let prev = document.getElementById('prev');
 let thumbnails = document.querySelectorAll('.thumbnail .item');
 
-// config param
+// Config parameters
 let countItem = items.length;
 let itemActive = 0;
-// event next click
-next.onclick = function(){
+let isUserScrolling = false; // Track if user is scrolling
+
+// Detect user scroll
+window.addEventListener('scroll', () => {
+    isUserScrolling = true; // User is scrolling
+    clearTimeout(scrollTimeout);
+    scrollTimeout = setTimeout(() => (isUserScrolling = false), 500); // Reset flag after 500ms of no scrolling
+});
+
+let scrollTimeout; // Timeout to reset the scroll state
+
+// Event next click
+next.onclick = function () {
     itemActive = itemActive + 1;
-    if(itemActive >= countItem){
+    if (itemActive >= countItem) {
         itemActive = 0;
     }
     showSlider();
-}
-//event prev click
-prev.onclick = function(){
+};
+
+// Event prev click
+prev.onclick = function () {
     itemActive = itemActive - 1;
-    if(itemActive < 0){
+    if (itemActive < 0) {
         itemActive = countItem - 1;
     }
     showSlider();
-}
-// auto run slider
+};
+
+// Auto-run slider
 let refreshInterval = setInterval(() => {
     next.click();
-}, 5000)
-function showSlider(){
-    // remove item active old
+}, 5000);
+
+function showSlider() {
+    // Remove item active old
     let itemActiveOld = document.querySelector('.slider .list .item.active');
     let thumbnailActiveOld = document.querySelector('.thumbnail .item.active');
     itemActiveOld.classList.remove('active');
     thumbnailActiveOld.classList.remove('active');
 
-    // active new item
+    // Activate new item
     items[itemActive].classList.add('active');
     thumbnails[itemActive].classList.add('active');
-    setPositionThumbnail();
+    
+    // Scroll thumbnails only if the user is not actively scrolling
+    if (!isUserScrolling) {
+        setPositionThumbnail();
+    }
 
-    // clear auto time run slider
+    // Clear auto-run slider timer
     clearInterval(refreshInterval);
     refreshInterval = setInterval(() => {
         next.click();
-    }, 5000)
+    }, 5000);
 }
-function setPositionThumbnail () {
+
+function setPositionThumbnail() {
     let thumbnailActive = document.querySelector('.thumbnail .item.active');
     let rect = thumbnailActive.getBoundingClientRect();
     if (rect.left < 0 || rect.right > window.innerWidth) {
@@ -52,10 +71,10 @@ function setPositionThumbnail () {
     }
 }
 
-// click thumbnail
+// Click thumbnail
 thumbnails.forEach((thumbnail, index) => {
     thumbnail.addEventListener('click', () => {
         itemActive = index;
         showSlider();
-    })
-})
+    });
+});
